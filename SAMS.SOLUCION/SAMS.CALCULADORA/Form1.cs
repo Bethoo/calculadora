f7ff4af6ef9413dcb3190edf7aa400560f924636
Bethoo <gonzho199s@gmail.com>
2017-01-24 19:35:59 -0600

    Add files via upload

---
+++ SAMS.SOLUCION/SAMS.CALCULADORA/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SAMS.CALCULADORA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = (float.Parse(textBox1.Text) + float.Parse(textBox2.Text)).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = (float.Parse(textBox1.Text) - float.Parse(textBox2.Text)).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = (float.Parse(textBox1.Text) * float.Parse(textBox2.Text)).ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = (float.Parse(textBox1.Text) / float.Parse(textBox2.Text)).ToString();
+        }
+        int b1 = 0;
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!((char.IsNumber(e.KeyChar)) || (char.IsPunctuation(e.KeyChar) && b1 == 0))&& (e.KeyChar != (char)Keys.Back))
+            {
+                MessageBox.Show("Solo se permiten numeros", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+            if (char.IsPunctuation(e.KeyChar))
+            {
+                b1 = 1;
+            }
+        }
+        int b = 0;
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            if (!((char.IsNumber(e.KeyChar)) || (char.IsPunctuation(e.KeyChar) && b==0)) && (e.KeyChar != (char)Keys.Back))
+            {
+                MessageBox.Show("Solo se permiten numeros", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+            if(char.IsPunctuation(e.KeyChar))
+            {
+                b = 1;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
